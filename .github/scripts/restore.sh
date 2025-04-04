5bbdd56477ac3d131cee9b05d817aa6862080b7c
Helio Frota <00hf11@gmail.com>
2025-04-04 13:41:09 -0300

    updates

--- .github/scripts/restore.sh
+++ .github/scripts/restore.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-ls -loshta
-pwd
-ls -loshta .trustify/postgres/
 psql_path="$HOME/work/trustify-api-hurl-tests/trustify-api-hurl-tests/.trustify/postgres/17.2.0/bin/psql"
 dump_path="$HOME/work/trustify-api-hurl-tests/trustify-api-hurl-tests/dump.sql"
 
