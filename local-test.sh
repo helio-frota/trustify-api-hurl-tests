d83b8db3daed0121adb54639ed9875ab63819268
Helio Frota <00hf11@gmail.com>
2025-05-15 13:33:04 -0300

    updates

--- local-test.sh
+++ local-test.sh
@@ -1,11 +1,11 @@
 hurl --test --jobs 1 --variables-file .env h/upload*.hurl
-hurl --test --jobs 1 --variables-file .env h/sbom.hurl
-hurl --test --jobs 1 --variables-file .env h/advisory.hurl
-hurl --test --jobs 1 --variables-file .env h/vulnerability.hurl
-hurl --test --jobs 1 --variables-file .env h/analysis.hurl
+# hurl --test --jobs 1 --variables-file .env h/sbom.hurl
+# hurl --test --jobs 1 --variables-file .env h/advisory.hurl
+# hurl --test --jobs 1 --variables-file .env h/vulnerability.hurl
+# hurl --test --jobs 1 --variables-file .env h/analysis.hurl
 hurl --test --jobs 1 --variables-file .env h/importer.hurl
-hurl --test --jobs 1 --variables-file .env h/product.hurl
-hurl --test --jobs 1 --variables-file .env h/organization.hurl
-hurl --test --jobs 1 --variables-file .env h/purl.hurl
-hurl --test --jobs 1 --variables-file .env h/other.hurl
+# hurl --test --jobs 1 --variables-file .env h/product.hurl
+# hurl --test --jobs 1 --variables-file .env h/organization.hurl
+# hurl --test --jobs 1 --variables-file .env h/purl.hurl
+# hurl --test --jobs 1 --variables-file .env h/other.hurl
 
